1cc5c9fa3a665f25dd8b905ebf39fe8dc9831b5b
Joseph Toh Djojo <jt641v@att.com>
2018-05-28 10:45:20 -0400

    install vim plugins

--- src/js/setups/vim.js
+++ src/js/setups/vim.js
@@ -2,6 +2,8 @@ const path = require('path')
 const os = require('os')
 const { execPromise, symlink } = require('../utils')
 
+const HOMEDIR = os.homedir()
+
 module.exports = () => {
   return execPromise('brew install vim --with-override-system-vi', 'Install latest Vim', false)
     .then(({err}) => {
@@ -11,12 +13,14 @@ module.exports = () => {
         Promise.resolve()
       }
     })
+    .then(() => execPromise(`rm -rf ${HOMEDIR}/.vim`, 'Empty .vim folder'))
     .then(() => execPromise('curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim', 'Install Vim-Plug'))
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim', 'Install Vim-Plug'))
     .then(() =>
       symlink(
         path.resolve(__dirname, '..', '..', 'dotfiles', 'vim', 'vimrc'),
-        `${os.homedir()}/.vimrc`
+        `${HOMEDIR}/.vimrc`
       )
     )
+    .then(() => execPromise(`cd ${HOMEDIR}/.vim/plugged/YouCompleteMe; vim -c "PlugInstall" -c "qa!"`, 'Install Vim plugins'))
 }
